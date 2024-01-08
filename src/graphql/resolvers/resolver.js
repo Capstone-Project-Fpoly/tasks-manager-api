1b2d3229e5af553cac4fbeb357b5ffca255a91b0
Thanh-PH20122 <thanhdvph20122@pft.edu.vn>
2024-01-08 17:23:41 +0700

    first commit

--- src/graphql/resolvers/resolver.js
+++ src/graphql/resolvers/resolver.js
@@ -2,7 +2,6 @@ const {books,authors} = require('../data/static')
 const resolvers = {
     Query: {
         books: () => books,
-        authors: () => authors,
         // book: (parent, args) =>
         //     books.find((book) => book.id == args.id),
         // author: (parent, args,data) => {
