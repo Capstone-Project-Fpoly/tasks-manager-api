962ed37899c61e1ec850ec725d91aa97737356c9
Thanh-PH20122 <thanhdvph20122@pft.edu.vn>
2024-01-18 16:06:06 +0700

    feature leave board

--- src/graphql/resolvers/Mutations/Board.Mutations.js
+++ src/graphql/resolvers/Mutations/Board.Mutations.js
@@ -1,54 +1,66 @@
 const BoardModel = require("../../../models/boardSchema");
+const CardModel = require("../../../models/cardShema");
+const ListModel = require("../../../models/listSchema");
 const auth = require("../authorization");
 
 class BoardMutations {
-    static createBoard = async (args, context) => {
-        const user = await auth(context.token);
-        const board = new BoardModel({
-            title: args.title,
-            isPublic: args.isPublic,
-            lists: [],
-            createdAt: new Date(),
-            users: [],
-            color: args.color ?? '168CD5',
-            ownerUser: user.uid,
-        })
+  static createBoard = async (args, context) => {
+    const user = await auth(context.token);
+    const board = new BoardModel({
+      title: args.title,
+      isPublic: args.isPublic,
+      lists: [],
+      createdAt: new Date(),
+      users: [],
+      color: args.color ?? "168CD5",
+      ownerUser: user.uid,
+    });
 
-        board.save()
-            .catch(err => { throw new Error(err) });
-        return board;
+    board.save().catch((err) => {
+      throw new Error(err);
+    });
+    return board;
+  };
+
+  static getBoards = async (args, context) => {
+    const user = await auth(context.token);
+    const boards = await BoardModel.find({
+      $or: [{ ownerUser: user.uid }, { users: user.uid }],
+    });
+
+    return boards;
+  };
+  static leaveBoard = async (args, context) => {
+    try {
+      const user = await auth(context.token);
+      const uid = user.uid;
+      const idBoard = args.idBoard;
+      const board = await BoardModel.findOne({ _id: idBoard });
+      if (board.users != 0) {
+        await BoardModel.updateOne({ _id: idBoard }, { $pull: { users: uid } });
+      }
+      if (board.lists.length == 0) return true;
+
+      const lists = await ListModel.find({ _id: { $in: board.lists } });
+
+      let allListIdCards = [];
+
+      for (const list of lists) {
+        if (list.cards && list.cards.length > 0) {
+          allListIdCards = allListIdCards.concat(list.cards);
+        }
+      }
+
+      if (allListIdCards.length == 0) return true;
+      await CardModel.updateMany(
+        { list: { $in: allListIdCards } },
+        { $pull: { users: uid } }
+      );
+      return true;
+    } catch (error) {
+      return false;
     }
-
-    static getBoards = async (args, context) => {
-        const user = await auth(context.token);
-        const boards = await BoardModel.find({
-            $or: [
-                { ownerUser: user.uid },
-                { users: user.uid }
-            ]
-        });
-
-        return boards;
-
-    }
-    static leaveBoard = async (args, context) => {
-        const user = await auth(context.token);
-        const uid = user.uid;
-        const idBoard = args.idBoard;
-        const board = await BoardModel.findOne({ _id: idBoard });
-        const resultBoard = await BoardModel.updateOne(
-            { _id: idBoard },
-            { $: { lists: uid } }
-        );
-        console.log(board);
-        console.log(resultBoard);
-        // const result = await BoardModel.updateOne(
-        //     { _id: idBoard },
-        //     { $pull: { users: uid } }
-        // );
-    }
-
+  };
 }
-
 
 module.exports = BoardMutations;
--- src/graphql/resolvers/authorization.js
+++ src/graphql/resolvers/authorization.js
@@ -1,10 +1,10 @@
-const jwt = require('jsonwebtoken');
-const UserModel = require('../../models/userSchema');
+const jwt = require("jsonwebtoken");
+const UserModel = require("../../models/userSchema");
 const auth = async (token) => {
-    if(!token) throw new Error('Đăng nhập mới được lấy data');
-    const decodedToken = jwt.verify(token, process.env.KEY);
-    const uid = decodedToken.uid;
-    const user = await UserModel.findOne({ uid });
-    return user;
-}
+  if (!token) throw new Error("Đăng nhập mới được lấy data");
+  const decodedToken = jwt.verify(token, process.env.KEY);
+  const uid = decodedToken.uid;
+  const user = await UserModel.findOne({ uid });
+  return user;
+};
 module.exports = auth;
