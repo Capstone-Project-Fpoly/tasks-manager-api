863fe68b32589ede97495f121ae2cc185cc18238
Thanh-PH20122 <thanhdvph20122@pft.edu.vn>
2024-01-23 17:45:27 +0700

    feature/logout

--- src/graphql/resolvers/getToken.js
+++ src/graphql/resolvers/getToken.js
@@ -1,14 +1,10 @@
 const jwt = require("jsonwebtoken");
 const UserModel = require("../../models/userSchema");
-const admin = require("firebase-admin");
 module.exports = async (parent, args, token) => {
   if (!args.email) throw new Error("Email không được để trống");
   const email = args.email;
   const user = await UserModel.findOne({ email });
   const uid = user.uid;
-  const userRecord = await admin.app();
-
-  console.log(userRecord);
-  // const jwtToken = jwt.sign({ uid }, process.env.KEY);
-  // return jwtToken;
+  const jwtToken = jwt.sign({ uid }, process.env.KEY);
+  return jwtToken;
 };
